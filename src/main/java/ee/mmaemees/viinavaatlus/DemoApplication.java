a2a3f783dcd238267e346d5d846488afa50442dd
Märt Mäemees <martmaemees@gmail.com>
2018-09-07 18:20:29 +0300

    Added spring dev-tools and checkstyle plugin

--- src/main/java/ee/mmaemees/viinavaatlus/DemoApplication.java
+++ src/main/java/ee/mmaemees/viinavaatlus/DemoApplication.java
@@ -5,16 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Main class for the application.
+ */
 @RestController
 @SpringBootApplication
 public class DemoApplication {
 
+    /**
+     * GET resource for testing.
+     * @return String "Hello world!"
+     */
     @RequestMapping("/")
     public String helloWorld() {
         return "Hello world!";
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    /**
+     * Main function for the application. Starts the Spring application.
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
